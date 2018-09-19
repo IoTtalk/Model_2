213a1dc89fec46e64a61a838744425a5c12d0689
Jyneda <jyneda@gmail.com>
2018-09-20 00:48:37 +0800

    Update custom.py

--- custom.py
+++ custom.py
@@ -1,23 +1,20 @@
 import DAN
 
-ServerIP = '210.61.8.56'   #=None:AutoSearch, or ='IP':Connect to this IP
+ServerIP = 'https://DomainName'   #=None:AutoSearch, or ='IP':Connect to this IP
 Comm_interval = 15 # unit:second
 
 def profile_init():
-    DAN.profile['dm_name']='SensorBox'
-    DAN.profile['d_name']= 'Wu2' #DAN.profile['dm_name']+'.'+DAN.get_mac_addr()[-4:]
+    DAN.profile['dm_name']='Model2'
+    DAN.profile['d_name']= 'Set2' #DAN.profile['dm_name']+'.'+DAN.get_mac_addr()[-4:]
 
 def odf():  # int only
     return []
 
 def idf():
     return [
-       ('AtPressure', float),
        ('Bugs', int),
-       ('Humidity', float),
        ('Moisture1', float),
        ('pH1', float),
-       ('Temperature', float),
        ('UV1', float),
        ('UV2', float),
     ]
